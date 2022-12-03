b7fe721178c1858500abef028e4618203d11a1e0
Charlie Calver <charlie.calver@genomicsplc.com>
2022-12-03 11:07:51 +0000

    day3 solution and tests

--- rucksacks.py
+++ rucksacks.py
@@ -4,15 +4,30 @@ LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
 
 def main():
-    rucksacks = read_input_file("resources/day_3_examples.txt")
-    common = find_common_item_in_rucksacks(rucksacks)
+    rucksacks = read_input_file("resources/day_3_actual.txt")
+    common = calculate_common_in_3_group(rucksacks)
     total = sum_items(common)
     print(total)
 
 
+def calculate_common_in_3_group(rucksacks):
+    common = []
+    buffer = []
+    for rucksack in rucksacks:
+        rucksack = rucksack.strip("\n")
+        buffer.append(rucksack)
+        if len(buffer) == 3:
+            intermediate = common_letters(buffer[0], buffer[1])
+            common.append(common_letters(intermediate, buffer[2]))
+            buffer = []
+    return common
+
+
 def halve(input_string):
+    # strip newline chars
+    input_string = input_string.strip("\n")
     half = int(len(input_string) / 2)
-    return input_string[: -(half + 1)], input_string[half:]
+    return input_string[:-half], input_string[half:]
 
 
 def common_letters(comparator_x, comparator_y) -> list:
--- test/test_rucksacks.py
+++ test/test_rucksacks.py
@@ -4,23 +4,58 @@ from rucksacks import *
 
 
 class TestDay3(unittest.TestCase):
-    def halve(self):
-        pass
+    def test_halve(self):
+        test_string = "vJrwpWtwJgWrhcsFMMfFFhFp"
+        first, second = halve(test_string)
+        self.assertEqual("vJrwpWtwJgWr", first)
+        self.assertEqual("hcsFMMfFFhFp", second)
+
+        test_string = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+        first, second = halve(test_string)
+        self.assertEqual("jqHRNqRjqzjGDLGL", first)
+        self.assertEqual("rsFMfFZSrLrFZsSL", second)
 
     def common_letters(self):
-        pass
+        test_string = "vJrwpWtwJgWrhcsFMMfFFhFp"
+        first, second = halve(test_string)
+        self.assertEqual(["p"], common_letters(first, second))
+
+        test_string = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+        first, second = halve(test_string)
+        self.assertEqual(["L"], common_letters(first, second))
 
     def find_common_item_in_rucksacks(self):
-        pass
+        common = find_common_item_in_rucksacks(
+            ["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"]
+        )
+        self.assertEqual([["p"], ["L"]], common)
 
     def score(self):
-        pass
+        self.assertEqual(1, "a")
+        self.assertEqual(2, "b")
+        self.assertEqual(27, "A")
 
     def sum_items(self):
-        pass
+        common = find_common_item_in_rucksacks(
+            ["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"]
+        )
+        self.assertEqual(54, sum_items(common))
 
     def test_example(self):
         rucksacks = read_input_file("resources/day_3_examples.txt")
         common = find_common_item_in_rucksacks(rucksacks)
         total = sum_items(common)
         self.assertEqual(157, total)
+
+    def test_example_part_2(self):
+        rucksacks = read_input_file("resources/day_3_examples.txt")
+        common = calculate_common_in_3_group(rucksacks)
+        total = sum_items(common)
+        self.assertEqual(70, total)
+
+    def test_calculate_common_in_3_group(self):
+        group = ["ab", "ac", "ad"]
+        self.assertEqual([["a"]], calculate_common_in_3_group(group))
+
+        group = ["ab", "ac", "ad", "ba", "bb", "bc"]
+        self.assertEqual([["a"], ["b"]], calculate_common_in_3_group(group))
